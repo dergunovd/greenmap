43072d1d8cb4a01a46ac448936110e2765ce361c
Dmitry Dergunov <dmitry@dergunov.net>
2018-09-22 06:18:13 +0400

    remove sagas

--- frontend/src/sagas/index.js
+++
@@ -1,7 +0,0 @@
-// import { takeEvery } from 'redux-saga';
-
-export function* watchedSagas() {
-  // yield takeEvery(FETCHED_SCENE, fetchSceneAsync);
-}
-
-export { watchedSagas as default };
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,5 +2,4 @@ import 'regenerator-runtime/runtime';
 import { createStore } from 'redux';
 import reducers from '../reducers/';
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default createStore(reducers);
